762ad54ddcfd412ae27d7bd8e0ff5848a06d7070
Claudia N <claudia.nicolae@oracle.com>
2023-02-09 09:42:32 +0200

    no unused vars update

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,7 +3,6 @@ import {
 	ShoppingIconContainer,
 	ItemCount,
 } from "./cart-icon.styles.jsx";
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
 
