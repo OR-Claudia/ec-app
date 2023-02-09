43f43c46950ed3f839597a9b9374738895766cbc
Claudia N <claudia.nicolae@oracle.com>
2023-02-09 16:29:18 +0200

    fix for item removal

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,3 +1,7 @@
+import { useContext } from "react";
+
+import { CartContext } from "../../contexts/cart.context";
+
 import {
 	CheckoutItemContainer,
 	ImageContainer,
@@ -6,35 +10,34 @@ import {
 	Arrow,
 	Value,
 	RemoveButton,
-} from "./checkout-item.styles.jsx";
-import { useContext } from "react";
-import { CartContext } from "../../contexts/cart.context";
+} from "./checkout-item.styles";
 
 const CheckoutItem = ({ cartItem }) => {
-	const { clearItemFromCart, addItemToCart, removeItemFromCart } = useContext(
+	const { name, imageUrl, price, quantity } = cartItem;
+
+	const { clearItemFromCart, addItemToCart, removeItemToCart } = useContext(
 		CartContext
 	);
 
 	const clearItemHandler = () => clearItemFromCart(cartItem);
 	const addItemHandler = () => addItemToCart(cartItem);
-	const removeItemHandler = () => removeItemFromCart(cartItem);
-
-	const { name, imageUrl, price, quantity } = cartItem;
+	const removeItemHandler = () => removeItemToCart(cartItem);
 
 	return (
 		<CheckoutItemContainer>
 			<ImageContainer>
 				<img src={imageUrl} alt={`${name}`} />
 			</ImageContainer>
-			<BaseSpan>{name}</BaseSpan>
+			<BaseSpan> {name} </BaseSpan>
 			<Quantity>
 				<Arrow onClick={removeItemHandler}>&#10094;</Arrow>
 				<Value>{quantity}</Value>
 				<Arrow onClick={addItemHandler}>&#10095;</Arrow>
 			</Quantity>
-			<BaseSpan>${price}</BaseSpan>
+			<BaseSpan> {price}</BaseSpan>
 			<RemoveButton onClick={clearItemHandler}>&#10005;</RemoveButton>
 		</CheckoutItemContainer>
 	);
 };
+
 export default CheckoutItem;
