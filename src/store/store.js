2a182c8527d3f38b8ffafe47950c7fc923cb363e
Claudia N <claudia.nicolae@oracle.com>
2023-02-15 15:59:56 +0200

    final

--- src/store/store.js
+++ src/store/store.js
@@ -1,30 +1,42 @@
 //combined place for redux reducers!
 import { compose, legacy_createStore, applyMiddleware } from "redux";
-// import logger from "redux-logger";
+import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-// const middleWares = [logger];
+const middleWares = [logger];
 
-const loggerMiddleware = (store) => (next) => (action) => {
-	if (!action.type) {
-		return next(action);
-	}
-	console.log("type", action.type);
-	console.log("payload", action.payload);
-	console.log("currentState", store.getState()); //value of the state right now
+// const loggerMiddleware = (store) => (next) => (action) => {
+// 	if (!action.type) {
+// 		return next(action);
+// 	}
+// 	console.log("type", action.type);
+// 	console.log("payload", action.payload);
+// 	console.log("currentState", store.getState()); //value of the state right now
 
-	next(action); //synchronous update action!
+// 	next(action); //synchronous update action!
 
-	console.log("nextState", store.getState());
+// 	console.log("nextState", store.getState());
+// };
+
+const persistConfig = {
+	key: "root",
+	storage,
+	blacklist: ["user"], //this can conflict and clash with the persistence of data
 };
 
-const composedEnhancers = compose(applyMiddleware(loggerMiddleware));
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const composedEnhancers = compose(applyMiddleware(middleWares));
 //middleware stays between components and store
 //dispatch > middleware > reducer/redux
 //for two middlewares dispatch > middleware 1 next() > middleware 2 next() > redux
 
 export const store = legacy_createStore(
-	rootReducer,
+	persistedReducer,
 	undefined,
 	composedEnhancers
 );
+
+export const persistor = persistStore(store);
